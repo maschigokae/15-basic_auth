86aa259b6af0d5adec340ffceb63125c8022f9a0
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-08 23:11:01 -0800

    add auth routes test file, build out POST route test

---
+++ test/auth-route-test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const User = require('../model/user.js');
+
+mongoose.Promise = Promise;
+
+require('../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  username: 'testuser',
+  password: 'testing1234',
+  email: 'testuser@testsite.com'
+};
+
+describe('Auth Routes', function() {
+  describe('POST: /api/register', function() {
+    describe('with a valid body', function() {
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.post(`${url}/api/register`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.text).to.be.a('string');
+          done();
+        });
+      });
+    });
+  });
+});
