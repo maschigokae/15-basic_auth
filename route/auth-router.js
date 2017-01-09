ec323af91360ec860af5a0b0dbe745927eeaaa22
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-08 22:53:02 -0800

    build out POST and GET routes

--- route/auth-router.js
+++ route/auth-router.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const parseJSON = require('body-parser');
+const parseJSON = require('body-parser').json();
 const debug = require('debug')('tableaux:auth-router');
 const Router = require('express').Router;
 const basicAuth = require('../lib/basic-auth-middleware.js');
@@ -9,6 +9,27 @@ const User = require('../model/user.js');
 
 const authRouter = module.exports = Router();
 
-authRouter.post('/api/register');
+authRouter.post('/api/register', parseJSON, function(req, res, next) {
+  debug('POST: /api/register');
 
-authRouter.get('/api/login');
+  let password = req.body.password;
+  delete req.body.password;
+
+  let user = new User(req.body);
+
+  user.generatePasswordHash(password)
+  .then( user => user.save())
+  .then( user => user.generateToken())
+  .then( token => res.send(token))
+  .catch(next);
+});
+
+authRouter.get('/api/login', basicAuth, function(req, res, next) {
+  debug('GET: /api/login');
+
+  User.findOne({ username: req.auth.username })
+  .then( user => user.comparePasswordHash(req.auth.password))
+  .then( user => user.generateToken())
+  .then( token => res.send(token))
+  .catch(next);
+});
