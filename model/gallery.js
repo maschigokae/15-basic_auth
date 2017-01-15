5e8071314da2de27876fbaa7f2ca3c29420695a8
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-14 18:27:42 -0800

    build out Gallery model, schema details

--- model/gallery.js
+++ model/gallery.js
@@ -3,6 +3,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const gallerySchema = Schema({});
+const gallerySchema = Schema({
+  galleryName: { type: String, required: true },
+  description: { type: String, required: true },
+  created: { type: Date, required: true, default: Date.now },
+  userID: { type: Schema.Types.ObjectId, required: true }
+});
 
 module.exports = mongoose.model('gallery', gallerySchema);
