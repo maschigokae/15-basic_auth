fb2015b10b9ecf0d619a4498dbb22baccfdf7889
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-15 21:51:56 -0800

    build out PUT, DELETE routers for gallery router module

--- route/gallery-router.js
+++ route/gallery-router.js
@@ -12,7 +12,7 @@ const galleryRouter = module.exports = Router();
 
 // TODO: BUILD OUT GALLERY ROUTES
 
-galleryRouter.post('/api/gallery', bearerAuth, function(req, res, next) {
+galleryRouter.post('/api/gallery', bearerAuth, parseJSON, function(req, res, next) {
   debug('POST: /api/gallery');
 
   req.body.userID = req.user._id;
@@ -26,12 +26,37 @@ galleryRouter.get('/api/gallery/:id', bearerAuth, function(req, res, next) {
 
   Gallery.findById(req.params.id)
   .then( gallery => {
+    if (gallery === null) return next(createError(404, 'not found'));
     if (gallery.userID.toString() !== req.user._id.toString()) return next(createError(401, 'invalid user'));
     res.json(gallery);
   })
   .catch(next);
 });
 
-galleryRouter.put('/api/gallery/:id');
+galleryRouter.put('/api/gallery/:id', bearerAuth, parseJSON, function(req, res, next) {
+  debug('PUT: /api/gallery/:id');
 
-galleryRouter.delete('/api/gallery/:id');
+  if (Object.getOwnPropertyNames(req.body).length === 0) return next(createError(400, 'bad request'));
+  Gallery.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then( gallery => {
+    if (gallery === null) return next(createError(404, 'not found'));
+    if (gallery.userID.toString() !== req.user._id.toString()) return next(createError(401, 'invalid user'));
+    res.json(gallery);
+  })
+  .catch( err => {
+    if (err.name === 'ValidationError') return next(err);
+    next(createError(404, 'not found'));
+  });
+});
+
+galleryRouter.delete('/api/gallery/:id', bearerAuth, function(req, res, next) {
+  debug('DELETE: /api/gallery/:id');
+
+  Gallery.findByIdAndRemove(req.params.id)
+  .then( gallery {
+    if (gallery === null) return next(createError(404, 'not found'));
+    if (gallery.userID.toString() !== req.user._id.toString()) return next(createError(401, 'invalid user'));
+    res.status(204).send();
+  })
+  .catch( err => next(createError(404, 'not found')));
+});
