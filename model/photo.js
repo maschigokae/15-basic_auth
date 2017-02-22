0cbf2d5be66ff6438fbac278cb75987de494e04b
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-22 14:48:22 -0800

    build out Photo resource model

--- model/photo.js
+++ model/photo.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PhotoSchema = Schema({
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  userID: { type: Schema.Types.ObjectId, required: true },
+  galleryID: { type: String, required: true, unique: true },
+  imageURI: { type: String, required: true, unique: true },
+  objectKey: { type: String, required: true, unique: true },
+  created: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('photo', photoSchema);
