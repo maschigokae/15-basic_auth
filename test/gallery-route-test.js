ba770149192b0c5cd65661f860412b687921004c
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-22 12:09:53 -0800

    build out PUT route tests

--- test/gallery-route-test.js
+++ test/gallery-route-test.js
@@ -14,8 +14,14 @@ const url = `http://localhost:${process.env.PORT}`;
 
 mongoose.Promise = Promise;
 
-const exampleUser = {
-  username: 'testuser',
+const exampleUser1 = {
+  username: 'testuser1',
+  email: 'testuser@testsite.com',
+  password: 'testing1234'
+};
+
+const exampleUser2 = {
+  username: 'testuser2',
   email: 'testuser@testsite.com',
   password: 'testing1234'
 };
@@ -25,7 +31,12 @@ const exampleGallery = {
   description: 'test gallery description'
 };
 
-describe('Gallery Routes', function() {
+const updatedGallery = {
+  galleryName: 'updated gallery name',
+  description: 'updated gallery description'
+};
+
+describe('Gallery Routes (POST, GET)', function() {
   afterEach( done => {
     Promise.all([
       User.remove({}),
@@ -38,8 +49,8 @@ describe('Gallery Routes', function() {
   describe('POST: /api/gallery', function() {
     describe('with a valid body', () => {
       before( done => {
-        new User(exampleUser)
-        .generatePasswordHash(exampleUser.password)
+        new User(exampleUser1)
+        .generatePasswordHash(exampleUser1.password)
         .then( user => user.save())
         .then( user => {
           this.tempUser = user;
@@ -82,8 +93,8 @@ describe('Gallery Routes', function() {
 
     describe('with an invalid body and valid auth', function() {
       before( done => {
-        new User(exampleUser)
-        .generatePasswordHash(exampleUser.password)
+        new User(exampleUser1)
+        .generatePasswordHash(exampleUser1.password)
         .then( user => user.save())
         .then( user => {
           this.tempUser = user;
@@ -114,8 +125,8 @@ describe('Gallery Routes', function() {
   describe('GET: /api/gallery/:id', function() {
     describe('with a valid body', () => {
       before( done => {
-        new User(exampleUser)
-        .generatePasswordHash(exampleUser.password)
+        new User(exampleUser1)
+        .generatePasswordHash(exampleUser1.password)
         .then( user => user.save())
         .then( user => {
           this.tempUser = user;
@@ -138,6 +149,10 @@ describe('Gallery Routes', function() {
           done();
         })
         .catch(done);
+      });
+
+      after( () => {
+        delete exampleGallery.userID;
       });
 
       it('should return a gallery', done => {
@@ -169,6 +184,108 @@ describe('Gallery Routes', function() {
     describe('with an invalid id and valid auth', () => {
       it('should return a 404 \'not found\' error', done => {
         request.get(`${url}/api/gallery/invalid-id`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+});
+
+describe('Gallery Routes (PUT)', function() {
+  afterEach( done => {
+    Promise.all([
+      User.remove({}),
+      Gallery.remove({})
+    ])
+    .then( () => done())
+    .catch(done)
+  });
+
+  describe('PUT: /api/gallery/:id', function() {
+
+    before( done => {
+      new User(exampleUser2)
+      .generatePasswordHash(exampleUser2.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    before( done => {
+      exampleGallery.userID = this.tempUser._id.toString();
+      new Gallery(exampleGallery).save()
+      .then( gallery => {
+        this.tempGallery = gallery;
+        done();
+      })
+      .catch(done);
+    });
+
+    after( () => {
+      delete exampleGallery.userID;
+    });
+
+    describe('with a valid id and body', () => {
+      it('should return an updated gallery', done => {
+        request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+        .send(updatedGallery)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.galleryName).to.equal(updatedGallery.galleryName);
+          expect(res.body.description).to.equal('updated gallery description');
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid body and valid auth', () => {
+      it('should return a 400 \'bad request\' error', done => {
+        request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+        .send({})
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid body and valid auth', () => {
+      it('should return a 401 \'unauthorized\' error', done => {
+        request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+        .send(updatedGallery)
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(401);
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid id and valid auth', () => {
+      it('should return a 404 \'not found\' error', done => {
+        request.put(`${url}/api/gallery/invalid-id`)
+        .send(updatedGallery)
         .set({
           Authorization: `Bearer ${this.tempToken}`
         })
