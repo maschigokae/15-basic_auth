654a084140a7117c3e62f75030690f471422c660
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-22 16:06:57 -0800

    refactor galleryID property on Photo model

--- model/photo.js
+++ model/photo.js
@@ -7,7 +7,7 @@ const PhotoSchema = Schema({
   name: { type: String, required: true },
   desc: { type: String, required: true },
   userID: { type: Schema.Types.ObjectId, required: true },
-  galleryID: { type: String, required: true, unique: true },
+  galleryID: { type: Schema.Types.ObjectId, required: true },
   imageURI: { type: String, required: true, unique: true },
   objectKey: { type: String, required: true, unique: true },
   created: { type: Date, default: Date.now }
